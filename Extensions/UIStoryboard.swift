470394e873b21f83c9d9763170d3e48a31d34483
Kyryl Nevedrov <kyryl.nevedrov@vedidev.com>
2017-04-17 16:21:34 +0300

    delete unnecessary files

--- Extensions/UIStoryboard.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import UIKit
-
-extension UIStoryboard {
-
-    func instantiateViewController<T: UIViewController>() -> T where T: ReusableView {
-        return instantiateViewController(withIdentifier: T.reuseIdentifier) as! T
-    }
-}
